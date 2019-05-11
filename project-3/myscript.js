181cd4f44e0adda054e2468ffdcacd623878e9a4
Meri Engel - Programming Instructor McHenry County College <34421299+meriengel@users.noreply.github.com>
2019-05-11 13:35:06 -0700

    Add files via upload

---
+++ project-3/myscript.js
@@ -0,0 +1,54 @@
+var myNumber = 10;
+// create a string variable named myString and assign a value to it
+
+// create a boolean variable named myBool and assign a value to it
+
+
+
+
+// Change the contents of the paragrpahs with ID's to display the variables
+
+// sample: number
+document.getElementById("number").innerHTML = myNumber;
+// string
+
+// bool
+
+
+// finish creating variables: a, b, c, and d and assign whole number values to them
+var a = 10;
+var b = 20;
+
+
+// demonstrate addition and set the result to the add paragraph
+document.getElementById("add").innerHTML = a + " + " + a + " = " + (a + a);
+
+// demonstrate subtraction and set the result to the subtract paragraph
+
+// demonstrate multiplication and set the result to the  multiply paragraph
+
+// demonstrate division and set the result to the division paragraph
+
+// Conditional logic Sample:
+
+if (a > b) { document.getElementById("condition1").innerHTML = a + " is greater than " + b; }
+else { document.getElementById("condition1").innerHTML = a + " is less than " + b; }
+
+// provide 3 other examples of logic using ==, ===, and != and the variables a, b, c & d
+
+// provide a logic example using and; display the result in the and paragraph
+
+// provide a logic example using or and display the result in the or paragraph
+
+
+// Create an array named cars containing 6 car names
+
+
+
+// display the entire array in the paragraph array
+
+
+
+// display the value in the third slot in the array in the paragraph array3
+
+// reverse the array and dispaly it in the paragraph reverse
