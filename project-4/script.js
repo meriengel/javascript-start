05d664e82b7c3f024a9a642c51d77e9f547c9809
Meri Engel - Programming Instructor McHenry County College <34421299+meriengel@users.noreply.github.com>
2019-05-12 12:54:58 -0700

    Add files via upload

---
+++ project-4/script.js
@@ -0,0 +1,51 @@
+// Write a named function that accepts a first name, a last name, and the paragraph id they should display to.
+
+
+
+
+
+
+// call the function four times, passing it different first names, last names, and the id of name1,
+// name2, name3, and name4
+
+
+// Write a function that recieves a dollar amount and a tip percentage
+// and returns the dollar amount of the tip
+
+
+
+
+
+//call the tipping function twice, storing the return values in the variables 
+// tip1 and tip2
+
+
+document.getElementById("tip1").innerHTML = tip1
+document.getElementById("tip2").innerHTML = tip2
+
+
+// using the two variables below 
+// write an anonymous function that stores the 
+// amount of sales tax in a var salestax
+var total = 1234.5
+var tax = 0.6
+
+document.getElementById("sales").innerHTML = salestax;
+
+
+// create a function that contains local variables
+// carbs, fat, and protein. Assign numeric values to them.
+// calculate calories for the day 
+// (protein * 4) + (carbs * 4 ) + (fat*9)
+// display the results in the calories paragraph
+
+
+
+
+
+// create a constant named pi that holds the first 4 digits of pi
+
+
+// create a function that calculates the area of a circle using the constant pi and
+// an internal let variable named radius
+// display the results in the cirle paragraph
