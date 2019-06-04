6020ebddefbd53d40fd78543c3fb208f419da4d2
Meri Engel - Programming Instructor McHenry County College <34421299+meriengel@users.noreply.github.com>
2019-06-03 22:38:18 -0500

    Uploaded start files

---
+++ project-6/script.js
@@ -0,0 +1,37 @@
+/*  Access and change elements
+    Change the text of the h2 tag 
+    so that it displays the current date
+    change the text of the h1 to your name, project 6
+    using the querySelector command
+*/
+
+
+/* Access and change classes
+   add the class green to the head-title
+   remove the class purple from the head-title
+
+*/
+
+
+/*  Access and Change attributes
+    change the ID of h3 from 
+    subtitle to section-title
+    using querySelector
+
+*/
+
+
+
+/* 
+    Adding DOM Elements
+    Following along with the video, use either example
+    to add a figcaption to the main-image figure containing
+    the kitty.jpg picture, use the alt text for the caption
+*/
+
+
+
+/* Inline Styles
+    add three in line styles to elements of your choice.
+    You may add to or change the HTML to support this
+*/
