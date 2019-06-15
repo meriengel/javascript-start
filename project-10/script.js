9ffd8c96a8eb9997dc516db993e334fa667e7a25
Meri Engel - Programming Instructor McHenry County College <34421299+meriengel@users.noreply.github.com>
2019-06-15 14:48:29 -0500

    Add files via upload

---
+++ project-10/script.js
@@ -0,0 +1,35 @@
+/*
+    Create a variable to hold a string
+    Use loops to add each line of 99 bottles of beer to the wall to that string 
+    display in the #beer paragraph - look at prior assignments if you need help displaying
+    to a paragraph with an ID
+    It should look like this: 
+
+    99 bottles of beer on the wall
+    99 bottles of beer
+    Take one down, pass it around
+    98 bottles of beer on the wall
+
+    98 bottles of beer on the wall
+    98 bottles of beer
+    Take one down, pass it around
+    97 bottles of beer on the wall
+    
+    Make sure you get the new line by including <br> in the string you add to the variable.
+*/
+
+
+
+/* 
+    Create an array that holds the months of the year ("January", "February"....)
+    Create a monthsOut variable
+    Use loops to add the month and new line to the mothsOut variable
+    display in the #months paragraph
+*/
+
+
+/* 
+    Create a loop that will check to see if a number between
+    1 and 100 is even. Print the even numbers, use continue
+    to skip the odd numbers.
+*/
